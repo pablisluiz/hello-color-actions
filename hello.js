4650f23c44c592d52a49ac8eb38b9d4a0666729e
pablisluiz <pablis@gmail.com>
2024-10-28 18:54:06 +0100

    cambio de mensaje hello - vini

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hola Vinicius, te quedaste sin balon de oro jaj";
 console.log(hello());
 exports.hello = hello;
