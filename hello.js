a4cbcfde71b345c70072960fc6c507c8ef681bc8
pablisluiz <pablis@gmail.com>
2024-10-28 20:10:10 +0100

    baila vini

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hola Vinicius, te quedaste sin balon de oro jaj";
+const hello = () => "Hola Vinicius, te quedaste sin balon de oro jajaj, baila vini baila";
 console.log(hello());
 exports.hello = hello;
