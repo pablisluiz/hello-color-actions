dff764afc1a9d7dbae3dc0f90540b4405c2840d7
pablisluiz <110827010+pablisluiz@users.noreply.github.com>
2024-11-06 20:01:50 +0100

    Update hello.test.js

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hola Vinicius, te quedaste sin balon de oro jajaj, baila vini baila");
+    expect(hello.hello()).toEqual("Animo Valencia");
   });
 });
